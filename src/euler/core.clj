1439aea3b86fb315f1ddaf2d8159ab349db16d04
Istvan <leccine@gmail.com>
2012-08-28 10:54:16 -0700

    Breaking the lines

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,12 +1,23 @@
 ;Copyright (C) 666, /dev/null
 ;
-;Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+;Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+;and associated documentation files (the "Software"), to deal in the Software without restriction, 
+;including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+;and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+;subject to the following conditions:
 ;
-;The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+;The above copyright notice and this permission notice 
+;shall be included in all copies or substantial portions of the Software.
 ;
-;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+;EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+;OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+;IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+;DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+;TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+;WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ;
-;
+;And more importantly it turned out that lawyers has smaller penis than the normal sample.
 ;
 (ns euler23 
   ;euler23 - Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
